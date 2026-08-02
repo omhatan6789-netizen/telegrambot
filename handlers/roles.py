cd5450b776716c0b0cc6e56efe3e00f51642ace7
omhatan6789-netizen <omhatan6789@gmail.com>
2026-08-02 19:37:00 +0000

    حفظ

--- handlers/roles.py
+++ handlers/roles.py
@@ -90,7 +90,7 @@ async def roles_command(
     text = update.message.text
     user = update.effective_user
 
-    print(repr(text))
+    print("COMMAND:", repr(text))
     # =================
     # رتبتي
     # =================
