c8e1389d11ec5a6793fd0313eaf4b779984e5a1a
omhatan6789-netizen <omhatan6789@gmail.com>
2026-09-20 07:44:17 +0300

    حفظ أطلق بوتت

--- main.py
+++ main.py
@@ -903,9 +903,7 @@ def main():
 
     app.add_handler(
         MessageHandler(
-            filters.ChatType.GROUPS
-            & filters.TEXT
-            & ~filters.COMMAND,
+            filters.ChatType.GROUPS,
             check_muted_message
         ),
         group=-30
